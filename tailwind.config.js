64fb25f40414f33721cf8b7647e73be2b7079a21
hectorz <otefrajav@gmail.com>
2025-06-26 22:55:29 -0600

    Astro ta chido bro

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: ["./src/**/*.{astro,html,js,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        utng: "#6CC7AE"
+      },
+      fontFamily: {
+        sans: ['"Montserrat Variable"', "sans-serif"]
+      }
+    }
+  },
+  plugins: [],
+};
